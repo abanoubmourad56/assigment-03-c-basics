2f0ab1adf0dfd9a037cc83cef16fb29b5e2ce0b6
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-25 16:44:06 +0300

    question 2

--- assigment 03 c# basics/Program.cs
+++ assigment 03 c# basics/Program.cs
@@ -1,26 +1,40 @@
-﻿namespace assigment_03_c__basics
+﻿using System.Drawing;
+using System.Dynamic;
+
+namespace assigment_03_c__basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //1. Create Book class, object, store it in object, then print
-        //    public class Book
-        //{
-        //    public string Title { get; set; }
-        //    public int Pages { get; set; }
-        //}
+            //    public class Book
+            //{
+            //    public string Title { get; set; }
+            //    public int Pages { get; set; }
+            //}
 
-        //Book book = new Book
-        //{
-        //    Title = "Clean Code",
-        //    Pages = 464
-        //};
+            //Book book = new Book
+            //{
+            //    Title = "Clean Code",
+            //    Pages = 464
+            //};
 
-        //object obj = book;
+            //object obj = book;
 
-        //Console.WriteLine(obj);
+            //Console.WriteLine(obj);
 
+            //Using the Book class above, print the result of calling ToString(), Equals() (compare book with itself), GetHashCode(), and GetType() on book
+            //Book book = new Book
+            //{
+            //    Title = "Clean Code",
+            //    Pages = 464
+            //};
+
+            //Console.WriteLine(book.ToString());
+            //Console.WriteLine(book.Equals(book));
+            //Console.WriteLine(book.GetHashCode());
+            //Console.WriteLine(book.GetType());
         }
     }
 }
